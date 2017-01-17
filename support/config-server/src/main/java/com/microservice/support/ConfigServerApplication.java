2be8c710e6730ab67180e4aa9c8ebdda234ca65f
xjhmiracle <miracle_xjh@163.com>
2017-01-17 12:56:21 +0800

    微服务示例

--- support/config-server/src/main/java/com/microservice/support/ConfigServerApplication.java
+++ support/config-server/src/main/java/com/microservice/support/ConfigServerApplication.java
@@ -2,12 +2,9 @@ package com.microservice.support;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
-import javax.annotation.PostConstruct;
 
 /**
  * Created by miracle on 2017/1/17.
@@ -19,21 +16,11 @@ import javax.annotation.PostConstruct;
  */
 @SpringCloudApplication
 @EnableConfigServer
-public class ConfigServerApplication implements CommandLineRunner {
+public class ConfigServerApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigServerApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
-    }
-
-    @Override
-    public void run(String... strings) throws Exception {
-        LOGGER.info("配置服务器开始启动...");
-    }
-
-    @PostConstruct
-    public void started() {
-        LOGGER.info("配置服务器启动完毕...");
     }
 }
